c28b5176455be18b6fd5f11fcbb791c066dd9a0a
YOG3SHWAR <kanhay8@gmail.com>
2021-10-02 12:56:02 +0530

    PrimeNumberTest

--- PrimeNumberTest.cpp
+++ PrimeNumberTest.cpp
@@ -5,7 +5,8 @@ bool isPrime(int n) {
     if (n <= 1)
         return false;
 
-    for (int i = 2; i < n; i++)
+    // time complexity O(sqrt(n))
+    for (int i = 2; i * i < n; i++)
         if (n % i == 0)
             return false;
 
@@ -13,8 +14,8 @@ bool isPrime(int n) {
 }
 
 int main() {
-    // int n;
-    // cin >> n;
-    cout << isPrime(7);
+    int n;
+    cin >> n;
+    cout << isPrime(n);
     return 0;
 }
